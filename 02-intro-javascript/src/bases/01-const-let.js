6054fd1d35c85f4707fd326b270e019689696c21
Gabriel Ravelo <gravelo@ks2.tech>
2025-02-28 16:53:47 -0400

    intro js

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,20 @@
+
+// variables y constantes
+
+const nombre = 'Gabriel';
+const apellido = 'Ravelo';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log( nombre, apellido, valorDado );
+
+if ( true ) {
+    // const nombre = 'Eduardo';
+    let valorDado = 6;
+    console.log(valorDado);
+    // console.log(nombre);
+}
+
+console.log(valorDado);
+
---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,7 @@
+
+const persona = {
+
+};
+
+console.log( persona );
+
