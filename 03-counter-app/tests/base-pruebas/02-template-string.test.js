de07c0b7c183292cb7308abef39392099781c83c
Gabriel Ravelo <ravelogabriel2@gmail.com>
2025-03-04 21:26:46 -0400

    testing base-pruebas

---
+++ 03-counter-app/tests/base-pruebas/02-template-string.test.js
@@ -0,0 +1,15 @@
+import { getSaludo } from "../../src/base-pruebas/02-template-string";
+
+
+describe('Pruebas en el archivo 02-template-string', () => {
+
+    test('getSaludo debe retornar "Hola Gabriel"', () => {
+
+        const name = 'Gabriel';
+        const message = getSaludo( name );
+
+        expect( message ).toBe(`Hola ${ name }`);
+    });
+
+});
+
