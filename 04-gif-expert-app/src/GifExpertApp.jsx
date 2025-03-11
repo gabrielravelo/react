70a9a852a4aad4230931ae5b2b5b101d02b24927
Gabriel Ravelo <gravelo@ks2.tech>
2025-03-11 18:55:14 -0400

    GifExpertApp done

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,46 +1,30 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 
 export const GifExpertApp = () => {
 
-  const [ categories, setCategories ] = useState([ 'One Piece', '2do elemento' ]);
+  const [ categories, setCategories ] = useState([ 'One Piece' ]);
 
   const onAddCategory = ( newCategory ) => {
-    // console.log(newCategory);
+    if ( categories.includes(newCategory) ) return;
+
     setCategories([ newCategory, ...categories ]);
     // setCategories( cat => [ ...cat, 'Valorant' ] );
   };
 
-  // console.log( categories );
-
   return (
     <>
-        {/* titulo */}
         <h1>GifExpertApp</h1>
 
-        {/* input */}
-        
         <AddCategory 
-          // setCategories={ setCategories }
           onNewCategory={ onAddCategory } 
         />
         
-        {/* <input /> */}
-
-        {/* Listado de Gif */}
-        
-        {/* <button onClick={ onAddCategory }>
-          Agregar
-        </button> */}
-        <ol>
-          { 
-            categories.map( category => {
-              return <li key={ category } >{ category }</li>
-            }) 
-          }
-        </ol>
-            {/* Gif item */}
+        { 
+            categories.map( category => ( <GifGrid key={ category } category={ category } /> )) 
+        }
 
         
     </>
