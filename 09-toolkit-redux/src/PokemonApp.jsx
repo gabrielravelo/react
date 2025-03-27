60369147635f14805aa441c5922004ebd4dac702
Gabriel Ravelo <ravelogabriel2@gmail.com>
2025-03-26 22:46:17 -0400

    thunks

---
+++ 09-toolkit-redux/src/PokemonApp.jsx
@@ -0,0 +1,15 @@
+
+export const PokemonApp = () => {
+    return (
+        <>
+            <h1>PokemonApp</h1>
+            <hr />
+
+            <ul>
+                <li>Hello</li>
+                <li>Hello</li>
+                <li>Hello</li>
+            </ul>
+        </>
+    )
+}
