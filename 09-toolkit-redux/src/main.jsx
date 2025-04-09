95c14784dcfd155cfba4e345c19f5c6ae54448cd
Gabriel Ravelo <ravelogabriel2@gmail.com>
2025-04-08 20:22:08 -0400

    RTK query

--- 09-toolkit-redux/src/main.jsx
+++ 09-toolkit-redux/src/main.jsx
@@ -5,12 +5,14 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { PokemonApp } from './PokemonApp.jsx';
+import { TodoApp } from './TodoApp.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={ store }>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </StrictMode>,
 )
